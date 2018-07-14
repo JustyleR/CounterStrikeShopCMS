2cbf8e79f9d4e5e5469a5d4f1b3ae3f76368e351
JustyleR <justyler1@gmail.com>
2018-07-14 04:35:35 +0300

    Delete footer.php

--- templates/bootstrap/structure/admin/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="pull-left footer">
-    <p class="col-md-12">
-    <hr class="divider">
-    System by: <a href="http://www.steamcommunity.com/id/JustyleRbAbY">JustyleR</a> &bull; <a href="https://github.com/JustyleR/smsCMS/">Report bugs</a>
-</p>
-</footer>
-</div>
-</body>
-</html>
